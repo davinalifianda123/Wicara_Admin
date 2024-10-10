10116db945ca128f50d4b4e6ab56d61ab70e6461
Irma_Innayah <irmainnayah@gmail.com>
2024-10-10 10:41:19 +0700

    makan

--- lib/main.dart
+++ lib/main.dart
@@ -54,31 +54,32 @@ class _MyHomePageState extends State<MyHomePage> {
           BottomNavigationBarItem(
             icon: _currentIndex == 0
                 ? _buildSelectedIcon("../images/pengaduan.png", "Pengaduan")
-                :_buildUnselectedIcon("../images/pengaduan.png", "Pengaduan"),
+                : _buildUnselectedIcon("../images/pengaduan.png", "Pengaduan"),
             label: '',
           ),
           BottomNavigationBarItem(
             icon: _currentIndex == 1
                 ? _buildSelectedIcon("../images/Rating.png", "Rating")
-                :_buildUnselectedIcon("../images/Rating.png", "Rating"),
+                : _buildUnselectedIcon("../images/Rating.png", "Rating"),
             label: '',
           ),
           BottomNavigationBarItem(
             icon: _currentIndex == 2
                 ? _buildSelectedIcon("../images/Beranda.png", "Beranda")
-                :_buildUnselectedIcon("../images/Beranda.png", "Beranda"),
+                : _buildUnselectedIcon("../images/Beranda.png", "Beranda"),
             label: '',
           ),
           BottomNavigationBarItem(
             icon: _currentIndex == 3
                 ? _buildSelectedIcon("../images/Kehilangan.png", "Kehilangan")
-                :_buildUnselectedIcon("../images/Kehilangan.png", "Kehilangan"),
+                : _buildUnselectedIcon(
+                    "../images/Kehilangan.png", "Kehilangan"),
             label: '',
           ),
           BottomNavigationBarItem(
             icon: _currentIndex == 4
                 ? _buildSelectedIcon("../images/Profile.png", "Profile")
-                :_buildUnselectedIcon("../images/Profile.png", "Profile"),
+                : _buildUnselectedIcon("../images/Profile.png", "Profile"),
             label: '',
           ),
         ],
@@ -177,13 +178,169 @@ class BerandaScreen extends StatelessWidget {
   }
 }
 
-class KehilanganScreen extends StatelessWidget {
+class KehilanganScreen extends StatefulWidget {
   const KehilanganScreen({super.key});
 
   @override
+  // ignore: library_private_types_in_public_api
+  _KehilanganScreenState createState() => _KehilanganScreenState();
+}
+
+class _KehilanganScreenState extends State<KehilanganScreen> {
+// Jumlah laporan yang diajukan
+  final List<Report> _reports = [
+    Report(
+      user: "Ahmad Farkhani",
+      date: "15/9/2024 20:00",
+      description:
+          "Kepada warga solime, tolong laptop saya hilang disekitaran muh. Bentuknya kayak digambar. Yang menemukan saya doakan masuk surga.",
+      imageUrl: "../images/laptop.png",
+      status: "Diajukan",
+      contact: "08972617722819",
+    ),
+    Report(
+      user: "Ahmad Farkhani",
+      date: "15/9/2024 20:00",
+      description:
+          "Kepada warga solime, tolong laptop saya hilang disekitaran muh. Bentuknya kayak digambar. Yang menemukan saya doakan masuk surga.",
+      imageUrl: "../images/laptop.png",
+      status: "Dalam Proses",
+      contact: "08972617722819",
+    ),
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Layar Kehilangan'),
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              color: Color(0xFF060A47),
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(30),
+                bottomRight: Radius.circular(30),
+              ),
+            ),
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+            child: SafeArea(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Spacer(
+                    flex: 2,
+                  ),
+                  const Text(
+                    'Kehilangan',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const Spacer(),
+                  Row(
+                    children: [
+                      IconButton(
+                        icon: const Icon(
+                          Icons.search,
+                          color: Colors.white,
+                        ),
+                        onPressed: () {},
+                      ),
+                      Stack(
+                        children: [
+                          IconButton(
+                            icon: const Icon(
+                              Icons.notifications,
+                              color: Colors.white,
+                            ),
+                            onPressed: () {},
+                          ),
+                          Positioned(
+                            right: 8,
+                            top: 8,
+                            child: Container(
+                              width: 8,
+                              height: 8,
+                              decoration: const BoxDecoration(
+                                color: Colors.red,
+                                shape: BoxShape.circle,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Pengaduan',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  Row(
+                    children: [
+                      Image.asset(
+                        '../images/Kalender.png',
+                      ),
+                      const SizedBox(height: 4),
+                      const Text(
+                        '  Terakhir Update : 1 September 2024',
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  const TabBarContainer(),
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: _reports.length, // Menyesuaikan jumlah laporan
+                      itemBuilder: (context, index) {
+                        final report = _reports[index]; // Mengambil data laporan
+                        return Card(
+                          margin: const EdgeInsets.all(8.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              ListTile(
+                                leading: CircleAvatar(
+                                  backgroundImage: AssetImage(report.imageUrl),
+                                ),
+                                title: Text(report.user),
+                                subtitle: Text(report.date),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(16.0),
+                                child: Text(report.description),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(16.0),
+                                child: Text('Kontak: ${report.contact}'),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
@@ -194,7 +351,49 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Center(
-      child: Text('Layar Profile'),
+      child: Text('Layar Profil'),
+    );
+  }
+}
+
+// Class Model untuk Laporan Kehilangan
+class Report {
+  final String user;
+  final String date;
+  final String description;
+  final String imageUrl;
+  final String status;
+  final String contact;
+
+  Report({
+    required this.user,
+    required this.date,
+    required this.description,
+    required this.imageUrl,
+    required this.status,
+    required this.contact,
+  });
+}
+
+class TabBarContainer extends StatelessWidget {
+  const TabBarContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const DefaultTabController(
+      length: 3,
+      child: Column(
+        children: [
+          TabBar(
+            labelColor: Colors.black,
+            tabs: [
+              Tab(text: 'Diajukan'),
+              Tab(text: 'Dalam Proses'),
+              Tab(text: 'Selesai'),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
