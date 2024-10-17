95d675bfd6621d5c9c37958e09ecaff28505e73a
Fajar <maulanafr.27@gmail.com>
2024-10-17 11:38:29 +0700

    Click on Card

--- lib/main.dart
+++ lib/main.dart
@@ -667,102 +667,93 @@ class ServiceDetailPage extends StatelessWidget {
                     physics: const NeverScrollableScrollPhysics(),
                     itemCount: 5, // Jumlah review yang ditampilkan, bisa diubah
                     itemBuilder: (context, index) {
-                      return Card(
-                        elevation: 2,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        margin: const EdgeInsets.only(bottom: 16),
-                        child: Padding(
-                          padding: const EdgeInsets.all(12.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Row(
-                                children: [
-                                  const CircleAvatar(
-                                    backgroundImage: AssetImage(
-                                      '../images/avatar_placeholder.png',
-                                    ), // Ganti dengan gambar avatar
-                                  ),
-                                  const SizedBox(width: 8),
-                                  const Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        'Melia Apriani', // Nama pengguna
-                                        style: TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                      ),
-                                      Text(
-                                        '15/04/23', // Tanggal ulasan
-                                        style: TextStyle(
-                                          color: Colors.grey,
-                                          fontSize: 12,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                  const Spacer(),
-                                  // Popup menu button
-                                  PopupMenuButton<String>(
-                                    onSelected: (value) {
-                                      if (value == 'detail') {
-                                        // Navigasi ke halaman detail rating
-                                        Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                            builder: (context) => const DetailRatingPage(),
-                                          ),
-                                        );
-                                      }
-                                    },
-                                    itemBuilder: (BuildContext context) {
-                                      return [
-                                        const PopupMenuItem(
-                                          value: 'detail',
-                                          child: Text('Lihat Detail'),
-                                        ),
-                                      ];
-                                    },
-                                  ),
-                                ],
+                      return InkWell(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => DetailRatingPage(
+                                reviewId: index, // Pass any data you want to `DetailRatingPage`
                               ),
-                              const SizedBox(height: 8),
-                              Row(
-                                children: List.generate(5, (index) {
-                                  return const Icon(
-                                    Icons.star,
-                                    color: Colors.orange,
-                                    size: 16,
-                                  );
-                                }),
-                              ),
-                              const SizedBox(height: 8),
-                              const Text(
-                                'Ac nya dingin beuttt...serasa di kutub mungkin lain kali bisa diganti AC nya jadi Angin Cepoi Cepoi xixixixixi',
-                                style: TextStyle(fontSize: 14),
-                              ),
-                              const SizedBox(height: 8),
-                              const Row(
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                children: [
-                                  Text(
-                                    '56 orang merasa ulasan ini berguna',
-                                    style: TextStyle(
-                                      color: Colors.grey,
-                                      fontSize: 12,
+                            ),
+                          );
+                        },
+                        hoverColor: Colors.blue.withOpacity(0.1), // Optional: hover color effect
+                        child: Card(
+                          elevation: 2,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          margin: const EdgeInsets.only(bottom: 16),
+                          child: Padding(
+                            padding: const EdgeInsets.all(12.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  children: [
+                                    const CircleAvatar(
+                                      backgroundImage: AssetImage(
+                                        '../images/avatar_placeholder.png',
+                                      ), // Ganti dengan gambar avatar
                                     ),
-                                  ),
-                                ],
-                              ),
-                            ],
+                                    const SizedBox(width: 8),
+                                    const Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          'Melia Apriani', // Nama pengguna
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                        Text(
+                                          '15/04/23', // Tanggal ulasan
+                                          style: TextStyle(
+                                            color: Colors.grey,
+                                            fontSize: 12,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    const Spacer(),
+                                  ],
+                                ),
+                                const SizedBox(height: 8),
+                                Row(
+                                  children: List.generate(5, (index) {
+                                    return const Icon(
+                                      Icons.star,
+                                      color: Colors.orange,
+                                      size: 16,
+                                    );
+                                  }),
+                                ),
+                                const SizedBox(height: 8),
+                                const Text(
+                                  'Ac nya dingin beuttt...serasa di kutub mungkin lain kali bisa diganti AC nya jadi Angin Cepoi Cepoi xixixixixi',
+                                  style: TextStyle(fontSize: 14),
+                                ),
+                                const SizedBox(height: 8),
+                                const Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    Text(
+                                      '56 orang merasa ulasan ini berguna',
+                                      style: TextStyle(
+                                        color: Colors.grey,
+                                        fontSize: 12,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
                           ),
                         ),
                       );
                     },
-                  ),
+                  )
                 ],
               ),
             ),
@@ -774,7 +765,7 @@ class ServiceDetailPage extends StatelessWidget {
 }
 
 class DetailRatingPage extends StatelessWidget {
-  const DetailRatingPage({super.key});
+  const DetailRatingPage({super.key, required int reviewId});
 
   @override
   Widget build(BuildContext context) {
